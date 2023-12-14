174446c694390411d32c91e64917ec48c21a153a
AlexTheGeek <28763288+AlexTheGeek@users.noreply.github.com>
2023-12-14 17:20:08 +0100

    oups ça block

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, send_from_directory
+from flask import Flask, render_template, redirect, url_for, send_from_directory, request
 
 app = Flask(__name__, static_folder='static')
 
